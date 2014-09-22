2939c497f1e7103bffda175be568aaed3050f279
hex7c0 <carniellifrancesco@gmail.com>
2014-09-22 14:29:49 +0200

    update benchmarks

--- benchmark/body1-1kb.js
+++ benchmark/body1-1kb.js
@@ -16,19 +16,23 @@ global.string = getbuffer(1 * 1000).toString()
 
 var suite = new benchmark.Suite
 
+var encoding = [ 'hex', 'binary', 'base64' ];
 var hashes = require('crypto').getHashes();
 for (var i = 0, ii = hashes.length; i < ii; i++) {
-    suite.add({
-        name: 'string - ' + hashes[i],
-        minSamples: 50,
-        fn: 'var val = hash(string, "' + hashes[i] + '")'
-    });
-    suite.add({
-        name: 'buffer - ' + hashes[i],
-        minSamples: 50,
-        fn: 'var val = hash(buffer, "' + hashes[i] + '")'
-    });
-
+    for (var j = 0, jj = encoding.length; j < jj; j++) {
+        suite.add({
+            name: ' string-' + encoding[j] + '-' + hashes[i],
+            minSamples: 50,
+            fn: 'var val = hash(string, "' + hashes[i] + '", "' + encoding[j]
+                    + '")'
+        });
+        suite.add({
+            name: ' buffer-' + encoding[j] + '-' + hashes[i],
+            minSamples: 50,
+            fn: 'var val = hash(string, "' + hashes[i] + '", "' + encoding[j]
+                    + '")'
+        });
+    }
 }
 
 suite.on('start', function onCycle(event) {
@@ -44,7 +48,7 @@ suite.on('cycle', function onCycle(event) {
 suite.on('complete', function onComplete() {
 
     benchmarks.log();
-    console.log('Fastest is: ' + this.filter('fastest').pluck('name') + '\n');
+    console.log('Fastest is:' + this.filter('fastest').pluck('name') + '\n');
 })
 
 suite.run({
