5800961e8f7413dc45992295508c894d36e9efd2
hex7c0 <carniellifrancesco@gmail.com>
2015-02-07 16:44:13 +0100

    restyling

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 /**
  * @file nodejs-hash-performance main
  * @module nodejs-hash-performance
- * @package nodejs-hash-performance
  * @subpackage main
- * @version 0.0.4
+ * @version 0.0.0
  * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
  * @license GPLv3
@@ -14,12 +13,7 @@
  * initialize module
  */
 // import
-try {
-    var crypto = require('crypto');
-} catch (MODULE_NOT_FOUND) {
-    console.error(MODULE_NOT_FOUND);
-    process.exit(1);
-}
+var crypto = require('crypto');
 
 /*
  * functions
@@ -34,7 +28,8 @@ try {
  * @param {String} encoding - Check README.md
  * @return {Object}
  */
-module.exports = function hashes(raw, hash, encoding) {
+function hashes(raw, hash, encoding) {
 
-    return crypto.createHash(hash).update(raw).digest(encoding);
-};
+  return crypto.createHash(hash).update(raw).digest(encoding);
+}
+module.exports = hashes;
